4d34ad932a648973e0a5bd43c0704c62ee000f15
Tuno <ms.tung88@gmail.com>
2022-01-04 23:26:39 +0700

    update

--- question.js
+++ question.js
@@ -1,22 +1,18 @@
 const questions = [
     {
         "questionText":"What is Java?",
-        "answer":"Java is the high-level, object-oriented, robust, secure programming language, platform-independent, high performance, Multithreaded, and portable programming language. It was developed by James Gosling in June 1991. It can also be known as the platform as it provides its own JRE and API."
+        "answer":"Java is the high-level, object-oriented, robust, secure programming language, platform-independent, high performance, Multithreaded, and portable programming language. It was developed by James Gosling in June 1991. It can also be known as the platform as it provides its own JRE and API.",
+        "img":"image\001.png"
     },
     {
         "questionText":"What are the differences between C++ and Java?",
-        "answer":"The differences between C++ and Java are given in the following table.\n"
+        "answer":"The differences between C++ and Java are given in the following table.\n",
+        "img":"image\002.png"
     },
     {
         "questionText":"What is the difference between JDK, JRE, and JVM?",
-        "answer":"dvdcd"
-    },
-    {
-        "questionText":"Weeehat is Java3?",
-        "answer":"dvdcd"
-    },
-    {
-        "questionText":"Whajjjt is Java4?",
-        "answer":"dvdcd"
-    },
+        "answer":"dvdcd",
+        "img":"image\003.png"
+    }
+  
 ]
